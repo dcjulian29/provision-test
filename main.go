fc198b117c67c4f8bfdbc25cfeb1ea10193be45e
Julian Easterling <julian@julianscorner.com>
2024-01-01 00:11:23 -0500

    Converted a powershell/bash script to executable

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	switch len(os.Args) {
+	case 2:
+		if strings.EqualFold(os.Args[1], "ssh") {
+			run("vagrant", "ssh")
+			return
+		}
+
+		if strings.EqualFold(os.Args[1], "destroy") {
+			run("vagrant", "destroy", "--force")
+			return
+		}
+
+		run("vagrant", "destroy", "--force")
+		run("vagrant", "up")
+
+	case 3:
+		run("vagrant", "up")
+
+	default:
+		fmt.Println(color.RedString("Please provide host name to test or action (ssh or destroy)"))
+	}
+
+	provision(os.Args[1])
+}
+
+func provision(hostname string) {
+	count := "c"
+
+	if runtime.GOOS == "windows" {
+		count = "n"
+	}
+
+	err := run("ping", fmt.Sprintf("-%s", count), "1", "192.168.57.42")
+
+	if err == nil {
+		input, err := os.ReadFile("hosts.ini")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		err = os.WriteFile("hosts.test.ini", input, 0644)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		override := []byte(`
+
+[all:vars]
+ansible_host=192.168.57.42
+ansible_port=22
+ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o CheckHostIP=no'
+ansible_ssh_private_key_file=~/.ssh/insecure_private_key
+ansible_user=vagrant`)
+
+		file, err := os.OpenFile("hosts.test.ini", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		defer file.Close()
+
+		if _, err = file.Write(override); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		run("ansible-host", "provision", "--verbose", "--inventory", "./hosts.test.ini", hostname)
+
+		os.Remove("hosts.test.ini")
+	}
+}
+
+func run(binary string, params ...string) error {
+	cmd := exec.Command(binary, params...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+
+	return cmd.Run()
+}
